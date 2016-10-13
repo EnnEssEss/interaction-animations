6cc27b8a3a533fcf624f2716f102fda79ce682dd
Nicole <nicole.sturm@gmail.com>
2016-10-12 17:29:10 -0700

    Added initial work for basic tooltip

---
+++ src/tooltip-target.js
@@ -0,0 +1,73 @@
+(function ($, window, document, undefined) {
+    'use strict';
+
+    var pluginName = "tooltipTarget",
+        defaults = {
+        };
+
+    function TooltipTarget (element, options) {
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+
+        this.isActive = true;
+        this.$tooltipContainer = null;
+        this.originalToolTipSibling = null;
+
+        this.init();
+    }
+
+    $.extend(TooltipTarget.prototype, {
+        init: function () {
+            this.$tooltipContainer = $('<div class="tooltip-container"></div>')
+                .appendTo('body')
+                .html($(this.settings.selector).html());
+
+            this.bindEvents();
+        },
+        bindEvents: function () {
+            $(this.element)
+                .on('mouseover.'+this._name, TooltipTarget.prototype.showToolTip.bind(this))
+                .on('mouseout.'+this._name, TooltipTarget.prototype.hideToolTip.bind(this));
+        },
+        unbindEvents: function () {
+            $(this.element)
+                .off('.'+this._name);
+        },
+        showToolTip: function () {
+            if (!this.isActive) {
+                return;
+            }
+
+            var elemOffset = $(this.element).offset(),
+                elemHeight = $(this.element).height();
+            this.$tooltipContainer
+                .css({
+                    top: elemOffset.top + elemHeight,
+                    left: elemOffset.left
+                })
+                .show();
+        },
+        hideToolTip: function () {
+            this.$tooltipContainer.hide();
+        },
+        setActive: function (isActive) {
+            this.isActive = isActive;
+        },
+        destroy: function () {
+            this.unbindEvents();
+            this.$tooltipContainer.remove();
+            this.$tooltipContainer = null;
+        }
+    });
+
+    $.makePlugin(pluginName, TooltipTarget);
+
+    $(document).ready(function () {
+        $('[data-tooltip-target]').each(function () {
+            var element = $(this);
+            element[pluginName]({selector: element.data(pluginName)});
+        });
+    });
+})(jQuery, window, document);
